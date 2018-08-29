7c41174413649f8495a208bc06e0594d985d7aae
Jérémie Robert <appydo+jerob@gmail.com>
2018-08-29 21:06:06 +0200

    Install ispconfig at first launch for better configuration

--- start.sh
+++ start.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
 # DOCKERPASS=$(openssl rand -base64 32)
-# echo "ROOT password : $DOCKERPASS"
-# echo "root:pass"|chpasswd
 # sed -i 's/PermitRootLogin without-password/PermitRootLogin yes/g' /etc/ssh/sshd_config
+
+if [ ! -f /usr/local/ispconfig/interface/lib/config.inc.php ]; then
+	mysql_install_db
+	service mysql start \
+	&& echo "UPDATE mysql.user SET Password = PASSWORD('pass') WHERE User = 'root';" | mysql -u root \
+	&& echo "UPDATE mysql.user SET plugin='mysql_native_password' where user='root';" | mysql -u root \
+	&& echo "DELETE FROM mysql.user WHERE User='';" | mysql -u root \
+	&& echo "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');" | mysql -u root \
+	&& echo "DROP DATABASE IF EXISTS test;" | mysql -u root \
+	&& echo "DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';" | mysql -u root \
+	&& echo "FLUSH PRIVILEGES;" | mysql -u root
+	sed -i "s/^hostname=server1.example.com$/hostname=$HOSTNAME/g" /root/ispconfig3_install/install/autoinstall.ini
+	# RUN mysqladmin -u root password pass
+	service mysql start && php -q /root/ispconfig3_install/install/install.php --autoinstall=/root/ispconfig3_install/install/autoinstall.ini
+	# Directory for dump SQL backup
+	rm -r /root/ispconfig3_install
+fi
+
 screenfetch
 
 if [ ! -z "$DEFAULT_EMAIL_HOST" ]; then
@@ -20,9 +36,4 @@ fi
 if [ ! -z "$HOSTNAME" ]; then
 sed -i "s/^hostname=server1.example.com$/hostname=$HOSTNAME/g" /tmp/ispconfig3_install/install/autoinstall.ini
 fi
-# php -q /tmp/ispconfig3_install/install/install.php --autoinstall=/tmp/ispconfig3_install/install/autoinstall.ini
 /usr/bin/supervisord -c /etc/supervisor/supervisord.conf
-if [ ! -f /usr/local/ispconfig/interface/lib/config.inc.php ]; then
-	php -q /tmp/ispconfig3_install/install/install.php --autoinstall=/tmp/ispconfig3_install/install/autoinstall.ini
-	killall apache2
-fi
