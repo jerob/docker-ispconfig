259ce2bd6dfcc784c34f9832b1fa285f71c3e0a1
Jérémie Robert <appydo+jerob@gmail.com>
2018-08-29 22:41:20 +0200

    Reorder configuration ispconfig

--- start.sh
+++ start.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 # DOCKERPASS=$(openssl rand -base64 32)
 # sed -i 's/PermitRootLogin without-password/PermitRootLogin yes/g' /etc/ssh/sshd_config
-
+if [ ! -z "$DEFAULT_EMAIL_HOST" ]; then
+	sed -i "s/^\(DEFAULT_EMAIL_HOST\) = .*$/\1 = '$MAILMAN_EMAIL_HOST'/g" /etc/mailman/mm_cfg.py
+	newlist -q mailman $(MAILMAN_EMAIL) $(MAILMAN_PASS)
+	newaliases
+fi
+if [ ! -z "$LANGUAGE" ]; then
+	sed -i "s/^language=en$/language=$LANGUAGE/g" /root/ispconfig3_install/install/autoinstall.ini
+fi
+if [ ! -z "$COUNTRY" ]; then
+	sed -i "s/^ssl_cert_country=AU$/ssl_cert_country=$COUNTRY/g" /root/ispconfig3_install/install/autoinstall.ini
+fi
+if [ ! -z "$HOSTNAME" ]; then
+	sed -i "s/^hostname=server1.example.com$/hostname=$HOSTNAME/g" /root/ispconfig3_install/install/autoinstall.ini
+fi
 if [ ! -f /usr/local/ispconfig/interface/lib/config.inc.php ]; then
 	mysql_install_db
 	service mysql start \
@@ -21,19 +34,4 @@ fi
 
 screenfetch
 
-if [ ! -z "$DEFAULT_EMAIL_HOST" ]; then
-sed -i "s/^\(DEFAULT_EMAIL_HOST\) = .*$/\1 = '$MAILMAN_EMAIL_HOST'/g" /etc/mailman/mm_cfg.py
-newlist -q mailman $(MAILMAN_EMAIL) $(MAILMAN_PASS)
-newaliases
-fi
-echo "START"
-if [ ! -z "$LANGUAGE" ]; then
-sed -i "s/^language=en$/language=$LANGUAGE/g" /tmp/ispconfig3_install/install/autoinstall.ini
-fi
-if [ ! -z "$COUNTRY" ]; then
-sed -i "s/^ssl_cert_country=AU$/ssl_cert_country=$COUNTRY/g" /tmp/ispconfig3_install/install/autoinstall.ini
-fi
-if [ ! -z "$HOSTNAME" ]; then
-sed -i "s/^hostname=server1.example.com$/hostname=$HOSTNAME/g" /tmp/ispconfig3_install/install/autoinstall.ini
-fi
 /usr/bin/supervisord -c /etc/supervisor/supervisord.conf
